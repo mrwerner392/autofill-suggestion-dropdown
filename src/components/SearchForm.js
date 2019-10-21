16fb8e79080e8164004e37549dc52623c6b6e89e
mrwerner392 <mrwerner392@gmail.com>
2019-10-21 08:56:26 -0400

    keyDown logic

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -6,8 +6,13 @@ export default class SearchForm extends Component {
 
   state = {
     searchInput: '',
-    showSuggestions: false
+    showSuggestions: false,
+    activeSuggestion: 0,
+    suggestions: [],
+    suggestionsKeyHit: null
   }
+
+  suggestionsBox = React.createRef();
 
   onChange = evt => {
     this.setState({
@@ -17,6 +22,59 @@ export default class SearchForm extends Component {
 
   onSubmit = evt => {
     evt.preventDefault();
+  };
+
+  onKeyDown = evt => {
+    let { state: {activeSuggestion,showSuggestions},
+          props: {suggestions} } = this.state;
+    // console.log(evt.target);
+    // console.log(evt.currentTarget);
+
+    if (evt.keyCode === 13) {
+      // enter key
+      this.setState({
+        activeSuggestion: 0,
+        showSuggestions: false,
+        teamInput: suggestions[activeSuggestion],
+        suggestions: []
+      });
+
+    } else if (evt.keyCode === 27) {
+      // escape key
+      this.setState({
+        showSuggestions: false
+      })
+
+    } else if (evt.keyCode === 38) {
+      // up key
+      evt.preventDefault();
+      if (activeSuggestion !== 0) {
+        activeSuggestion--;
+        this.setState({
+          activeSuggestion,
+          suggestionsKeyHit: 'up'
+        });
+      } else {
+        this.setState({
+          showSuggestions: false
+        });
+      };
+
+    } else if (evt.keyCode === 40) {
+      // down key
+      evt.preventDefault();
+      if (showSuggestions === false && suggestions.length) {
+        this.setState({
+          showSuggestions: true,
+        });
+      } else if (activeSuggestion !== suggestions.length - 1) {
+        activeSuggestion++;
+        this.setState({
+          activeSuggestion,
+          suggestionsKeyHit: 'down'
+        });
+      };
+    };
   };
 
   getSuggestions = () => {
