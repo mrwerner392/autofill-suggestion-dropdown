6b598b8cd5cb1ffe15e6d9027c14e415e4f970a7
mrwerner392 <mrwerner392@gmail.com>
2019-10-20 21:24:03 -0400

    sample data

---
+++ src/data.js
@@ -0,0 +1,29 @@
+let sports = [
+  'Football',
+  'Baseball',
+  'Softball',
+  "Men's Ice Hockey",
+  "Women's Ice Hockey",
+  "Men's Golf",
+  "Women's Golf",
+  "Men's Basketball",
+  "Women's Basketball",
+  "Field Hockey",
+  "Men's Lacrosse",
+  "Women's Lacrosse",
+  "Swimming",
+  "Track & Field",
+  "Cross Country",
+  "Crew",
+  "Men's Soccer",
+  "Women's Soccer",
+  "Wrestling",
+  "Volleyball",
+  "Squash",
+  "Men's Tennis",
+  "Women's Tennis"
+]
+
+module.exports = {
+  sports
+}
